5741d61bbf885d06d76f20d964e8883fcc5f2d9f
ewaldvanrooyen-max <ewald.vanrooyen@gmail.com>
2025-08-22 18:18:12 +0200

    Create page.tsx

---
+++ site/app/posts/[...slug]/page.tsx
@@ -0,0 +1,21 @@
+import { getAllPosts, getPost } from "@/lib/posts";
+import { notFound } from "next/navigation";
+
+export default async function PostPage({ params }: { params: { slug: string[] } }) {
+  const post = await getPost(params.slug);
+  if (!post) return notFound();
+
+  return (
+    <article className="prose">
+      <h1>{post.meta.title}</h1>
+      {post.meta.date && <p><em>{new Date(post.meta.date).toDateString()}</em></p>}
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </article>
+  );
+}
+
+// Pre-generate all dynamic routes at build time
+export async function generateStaticParams() {
+  const posts = await getAllPosts();
+  return posts.map((p) => ({ slug: p.slug }));
+}
